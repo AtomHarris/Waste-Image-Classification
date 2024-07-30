fe5792ead98422ecd887a6b46faf635145f97e38
AtomHarris <harrislukundi@yahoo.com>
2024-07-30 23:46:26 +0300

    adding contact form

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ if selected == "Home":
                 with st.container(border=True):
                     if st.session_state.last_uploaded_image:
                         with st.spinner('Classifying...'):
-                            time.sleep(2)
+                            time.sleep(1)
 
                         st.success('Done!')
 
@@ -459,8 +459,9 @@ elif selected == "About":
 elif selected == "Contact":
     # Include FontAwesome CDN in your app
     st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">', unsafe_allow_html=True)
-    st.header("DEVELOPER CONTACTS", divider=True)
+    
     with st.container(border= True):
+        st.header("Developer Contacts", divider=True)
         col1, col2, col3 = st.columns(3)
         with col1:
             with st.container(border= True):
@@ -471,3 +472,39 @@ elif selected == "Contact":
         with col3:
             with st.container(border= True):
                 st.markdown('<i class="fas fa-user"></i> Harris Lukundi<br>[Email](mailto:harris.lukundi@student.moringaschool.com)<br>[Github](https://github.com/AtomHarris)', unsafe_allow_html=True)
+    with st.container(border=True):
+        # Contact Form
+        st.header("Contact Us", divider=True)
+        st.write("Please fill out the form below to send us a message.")
+        # Create placeholders for form fields
+        name_placeholder = st.empty()
+        email_placeholder = st.empty()
+        subject_placeholder = st.empty()
+        message_placeholder = st.empty()
+
+        with st.form(key='contact_form'):
+            
+            
+            # Use placeholders to hold the form fields
+            name = name_placeholder.text_input("Name")
+            email = email_placeholder.text_input("Email")
+            subject = subject_placeholder.text_input("Subject")
+            message = message_placeholder.text_area("Message")
+            
+            submit_button = st.form_submit_button("Send")
+            
+            if submit_button:
+                if name and email and subject and message:
+                    # Call your function to send an email
+                    # send_email(name, email, subject, message)
+                    
+                    # Show success message
+                    st.success("Your message has been sent successfully!")
+                    
+                    # Clear form fields using placeholders
+                    name_placeholder.text_input("Name", value="", key="name")
+                    email_placeholder.text_input("Email", value="", key="email")
+                    subject_placeholder.text_input("Subject", value="", key="subject")
+                    message_placeholder.text_area("Message", value="", key="message")
+                else:
+                    st.error("Please fill out all fields.")
